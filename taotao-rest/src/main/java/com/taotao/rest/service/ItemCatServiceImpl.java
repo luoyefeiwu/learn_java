525c0ecbfc15c29e510bf13cb494bcdb9903b9bd
jerry <568384493@qq.com>
2018-01-10 20:58:14 +0800

    添加 web层

---
+++ taotao-rest/src/main/java/com/taotao/rest/service/ItemCatServiceImpl.java
@@ -0,0 +1,18 @@
+package com.taotao.rest.service;
+
+import com.taotao.rest.pojo.ItemCatResult;
+
+/**
+ * 商品分类列表
+ * @author shengyan
+ *
+ */
+public class ItemCatServiceImpl implements ItemCatService {
+
+	@Override
+	public ItemCatResult getItemCatList() {
+		
+		return null;
+	}
+
+}
