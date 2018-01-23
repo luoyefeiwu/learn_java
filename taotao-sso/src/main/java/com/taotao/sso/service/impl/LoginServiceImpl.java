9b14ef61d36653d397b52242aa01d24f3c57852c
jerry <56838493@qq.com>
2018-01-23 23:11:24 +0800

    添加 单点登陆

--- taotao-sso/src/main/java/com/taotao/sso/service/impl/LoginServiceImpl.java
+++ taotao-sso/src/main/java/com/taotao/sso/service/impl/LoginServiceImpl.java
@@ -1,22 +1,73 @@
 package com.taotao.sso.service.impl;
 
+import java.util.List;
+import java.util.UUID;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.CookieUtils;
+import com.taotao.common.utils.JsonUtils;
+import com.taotao.mapper.TbUserMapper;
+import com.taotao.pojo.TbUser;
+import com.taotao.pojo.TbUserExample;
+import com.taotao.pojo.TbUserExample.Criteria;
+import com.taotao.sso.component.JedisClient;
 import com.taotao.sso.service.LoginService;
 
+@Service
 public class LoginServiceImpl implements LoginService {
 
-	/*登陆
-	 *  (non-Javadoc)
-	 * @see com.taotao.sso.service.LoginService#login(java.lang.String, java.lang.String, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	@Autowired
+	private TbUserMapper userMapper;
+	@Autowired
+	private JedisClient jedisClient;
+
+	@Value("${REDIS_SESSION_KEY}")
+	private String REDIS_SESSION_KEY;
+	@Value("${SESSION_EXPIRE}")
+	private Integer SESSION_EXPIRE;
+
+	/*
+	 * 登陆 (non-Javadoc)
+	 * 
+	 * @see com.taotao.sso.service.LoginService#login(java.lang.String,
+	 * java.lang.String, javax.servlet.http.HttpServletRequest,
+	 * javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
 	public TaotaoResult login(String username, String password, HttpServletRequest request,
 			HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
+		// 检验用户名和密码是否正确
+		TbUserExample example = new TbUserExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andUsernameEqualTo(username);
+		List<TbUser> list = userMapper.selectByExample(example);
+		if (list == null || list.isEmpty()) {
+			return TaotaoResult.build(400, "用户名或密码错误");
+		}
+		TbUser user = list.get(0);
+		// 校验密码
+		if (!user.getPassword().equals(DigestUtils.md5DigestAsHex(password.getBytes()))) {
+			return TaotaoResult.build(400, "用户名或密码错误");
+		}
+		// 生成Token
+		String token = UUID.randomUUID().toString();
+
+		// 将用户信息写入redis
+		user.setPassword(null);
+		jedisClient.set(REDIS_SESSION_KEY + ":" + token, JsonUtils.objectToJson(user));
+		// 设置过期时间
+		jedisClient.expire(REDIS_SESSION_KEY + ":" + token, SESSION_EXPIRE);
+		//写入cookie
+		CookieUtils.setCookie(request, response, "TT_TOKEN", token);
+		return TaotaoResult.ok(token);
 	}
 
 }
